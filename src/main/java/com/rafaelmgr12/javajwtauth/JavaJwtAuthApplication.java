cc96bb14470268b564555a0c2e304dfb94623ac4
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-10-08 14:07:59 -0300

    first commit

---
+++ src/main/java/com/rafaelmgr12/javajwtauth/JavaJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.javajwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJwtAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafaelmgr12/javajwtauth/JavaJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.javajwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJwtAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
